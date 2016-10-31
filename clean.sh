5348eff101c445ca400eefeb21ce0c5c53c1e67d
Ormin <orminapod3@gmail.com>
2016-10-31 20:57:15 +0100

    Clean

--- clean.sh
+++ clean.sh
@@ -1,5 +1,7 @@
-rm -rf ./Build/Artifacts/*
-rm -rf ./Build/Transpiled/*
+rm -rf ./Build/Artifacts/TIF/*
+rm -rf ./Build/Artifacts/Standalone/*
+rm -rf ./Build/Transpiled/TIF/*
+rm -rf ./Build/Transpiled/Standalone/*
 rm -rf ./Build/Workspace/*
 rm -rf ./Build/compile_log
 rm -rf ./Build/error_log
